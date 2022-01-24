7e74a8fbe8f257f6184afaf12f46a08320f86269
Luis Mora <luismora371@gmail.com>
2022-01-23 20:32:59 -0600

    parsing request bodies

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const server = http.createServer((req,res)=>{
     console.log(req.url, req.method, req.headers);
     // process.exit();
     const url = req.url;
+    const method = req.method;
+    const fs = require('fs');
 
     //ROUTING REQUEST
     if(url === '/'){
@@ -21,6 +23,21 @@ const server = http.createServer((req,res)=>{
         res.write('</html>');
         return res.end();
     }
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+          console.log(chunk);
+          body.push(chunk);
+        });
+        req.on('end', () => {
+          const parsedBody = Buffer.concat(body).toString();
+          const message = parsedBody.split('=')[1];
+          fs.writeFileSync('message.txt', message);
+        });
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      }
 
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
